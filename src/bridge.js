df84a1bd32105c24d28e1f479f80b9f8cf111424
lefeuvth <118385221+lefeuvth@users.noreply.github.com>
2026-04-23 13:32:19 +0200

    Fix https

--- src/bridge.js
+++ src/bridge.js
@@ -27,7 +27,7 @@ export async function initBridge() {
   return await Bridge.init({
     l1Url: "https://api.minascan.io/node/devnet/v1/graphql",
     l1ArchiveUrl: "https://api.minascan.io/archive/devnet/v1/graphql",
-    actionsApi: "http://api.actions.zeko.io/graphql",
+    actionsApi: "https://api.actions.zeko.io/graphql",
     zekoUrl: "https://testnet.zeko.io/graphql",
     zekoArchiveUrl: "https://archive.testnet.zeko.io/graphql",
     l1Network: "testnet",
